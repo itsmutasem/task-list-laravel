ef60014ef9db78ea2b7dfd50961014dc0e6a78cf
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-03-08 14:12:37 +0300

    All about routing

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -3,5 +3,17 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main page';
+});
+Route::get('/xxx', function (){
+    return 'hello';
+})->name('hello');
+Route::get('/hallo', function (){
+    return redirect()->route('hello');
+});
+Route::get('/greet/{name}', function ($name){
+    return 'Hello ' . $name . "!";
+});
+Route::fallback(function (){
+   return 'Still got somewhere!';
 });

6294308cf39c581e1def1edd5b2bace76cd562e2
Mutasem Mustafa <143445025+itsmutasem@users.noreply.github.com>
2025-03-15 15:28:57 +0300

    Validating and Sorting Data

--- task-list/routes/web.php
+++ task-list/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
@@ -11,7 +12,7 @@ Route::get('/', function (){
 
 Route::get('/tasks', function () {
     return view('index', [
-       'tasks' => \App\Models\Task::latest()->get()
+       'tasks' => Task::latest()->get()
     ]);
 })->name('tasks.index');
 
@@ -19,12 +20,23 @@ Route::view('/tasks/create', 'create')->name('tasks.create');
 
 Route::get('/tasks/{id}', function ($id) {
    return view('show', [
-       'task' => App\Models\Task::findOrFail($id)
+       'task' => Task::findOrFail($id)
    ]);
 })->name('tasks.show');
 
 Route::post('/tasks', function (Request $request) {
-    dd($request->all());
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+    $task = new Task;
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id]);
 })->name('tasks.store');
 
 Route::fallback(function (){
